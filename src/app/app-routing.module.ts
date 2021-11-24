c57f157aa0c3f6583c3c25f258c5d27142f04584
Ken Matthew Ammay <201910666@gordoncollege.edu.ph>
2021-11-24 23:29:23 +0800

    Added Http Client
    For Act 5

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -23,6 +23,22 @@ const routes: Routes = [
     path: 'profile',
     loadChildren: () => import('./profile/profile.module').then( m => m.ProfilePageModule)
   },
+  {
+    path: 'student-create',
+    loadChildren: () => import('./student-create/student-create.module').then( m => m.StudentCreatePageModule)
+  },
+  {
+    path: 'student-edit',
+    loadChildren: () => import('./student-edit/student-edit.module').then( m => m.StudentEditPageModule)
+  },
+  {
+    path: 'student-list',
+    loadChildren: () => import('./student-list/student-list.module').then( m => m.StudentListPageModule)
+  },
+  {
+    path: 'student-detail',
+    loadChildren: () => import('./student-detail/student-detail.module').then( m => m.StudentDetailPageModule)
+  },
 ];
 
 @NgModule({
